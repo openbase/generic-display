935ec62d4da6a77b08ee2d634e2d0dd2b500a427
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2016-02-29 12:58:48 +0100

    Remove dep constructor for DisplayRemote and implement remote broadcast pass through.

--- src/main/java/org/dc/display/DisplayRemote.java
+++ src/main/java/org/dc/display/DisplayRemote.java
@@ -10,12 +10,12 @@ package org.dc.display;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -51,11 +51,11 @@ public class DisplayRemote extends RSBRemoteService<UnitConfig> implements Displ
     private final DisplayRemote broadcastDisplayRemote;
 
     public DisplayRemote() {
-        this.broadcastDisplayRemote = new DisplayRemote(true);
+        this.broadcastDisplayRemote = new DisplayRemote(null);
     }
 
-    public DisplayRemote(final boolean broadcast) {
-        this.broadcastDisplayRemote = null;
+    public DisplayRemote(final DisplayRemote broadcastDisplayRemote) {
+        this.broadcastDisplayRemote = broadcastDisplayRemote;
     }
 
     @Override
