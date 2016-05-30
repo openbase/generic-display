b2ebd644269b2fec2a05d43f99331592c2422844
Divine Threepwood <DivineThreepwood@gmail.com>
2016-05-30 23:17:34 +0200

    add remote data class.

--- src/main/java/org/dc/display/DisplayRemote.java
+++ src/main/java/org/dc/display/DisplayRemote.java
@@ -10,12 +10,12 @@ package org.dc.display;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -51,10 +51,12 @@ public class DisplayRemote extends RSBRemoteService<UnitConfig> implements Displ
     private final DisplayRemote broadcastDisplayRemote;
 
     public DisplayRemote() {
+        super(UnitConfig.class);
         this.broadcastDisplayRemote = new DisplayRemote(null);
     }
 
     public DisplayRemote(final DisplayRemote broadcastDisplayRemote) {
+        super(UnitConfig.class);
         this.broadcastDisplayRemote = broadcastDisplayRemote;
     }
 
