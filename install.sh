29e3d96d28761a4b8f9c79f2df794854bff2129f
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2016-08-19 15:40:01 +0200

    test goal removed out of install script.

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ echo "=== clean ${APP_NAME} ===" &&
 mvn clean $@ &&
 clear &&
 echo "=== install and deploy ${APP_NAME} ===" &&
-mvn install -DassembleDirectory=${prefix} $@ &&
+mvn install -DskipTests -DassembleDirectory=${prefix} $@ &&
 clear &&
 echo "=== ${APP_NAME} is successfully installed to ${prefix} ==="
