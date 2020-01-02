6982472d003cf627dadfba95a96fd6c163225589
Divine Threepwood <divine@openbase.org>
2020-01-02 22:04:42 +0100

    update year in license header.

--- src/main/java/org/openbase/display/DisplayRemoteExample.java
+++ src/main/java/org/openbase/display/DisplayRemoteExample.java
@@ -4,7 +4,7 @@ package org.openbase.display;
  * #%L
  * GenericDisplay
  * %%
- * Copyright (C) 2015 - 2019 openbase.org
+ * Copyright (C) 2015 - 2020 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
